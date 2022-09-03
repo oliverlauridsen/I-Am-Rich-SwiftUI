b7df9485395924568dfcdc25ac9029b6e15468f5
Oliver Lauridsen <olidude2.0@gmail.com>
2022-09-03 15:03:10 +0200

    SwiftUI demo of functionality

--- I Am Rich SwiftUI/ContentView.swift
+++ I Am Rich SwiftUI/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich!")
+                    .font(.largeTitle)
+                    .padding()
+                    .foregroundColor(.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
+                                    
+                                    }
 }
